a806f82d1aa7a8f83e546ff8642252455d121f70
aishpasham <apasham2@illinois.edu>
2024-11-14 15:50:29 -0600

    Add database connection on backend

--- currents/server/api/main.py
+++ currents/server/api/main.py
@@ -4,20 +4,21 @@ from flask_cors import CORS
 # import sys
 # sys.path.append('../')
 
-# from db import get_natural_disaster
+from db import get_natural_disaster
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/data', methods=['GET', 'POST'])
-def receive_data():
-    data = request.data.decode("utf-8")
-    print("Hi")
-
-    if not data:
-        return jsonify({"error": "No data found"}), 404
-
-    return jsonify({"data": data}), 200
+res = get_natural_disaster('Joaquin')
+# @app.route('/', methods=['GET','POST'])
+# def receive_data():
+#     # data = request.data.decode("utf-8")
+#     # if not data:
+#     #     return jsonify({"error": "main.py: No data found"}), 404
+#     res = get_natural_disaster('Joaquin')
+#     print('res')
+#     return res, 200
+#     # return jsonify({"data": data}), 200
 
 if __name__ == '__main__':
     app.run()
--- currents/server/db.py
+++ currents/server/db.py
@@ -1,33 +1,37 @@
 import os
+from flask import Flask, jsonify
+from google.cloud.sql.connector import Connector
 import pymysql
-from flask import jsonify
+import sqlalchemy
 
 db_user = os.environ.get('CLOUD_SQL_USERNAME')
 db_password = os.environ.get('CLOUD_SQL_PASSWORD')
 db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
 db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
+connector = Connector()
 
-def open_connection():
-    unix_socket = '/cloudsql/{}'.format(db_connection_name)
-    try:
-        if os.environ.get('GAE_ENV') == 'standard':
-            conn = pymysql.connect(user=db_user, password=db_password,
-                                unix_socket=unix_socket, db=db_name,
-                                cursorclass=pymysql.cursors.DictCursor
-                                )
-    except pymysql.MySQLError as e:
-        print(e)
+def getconn() -> pymysql.connections.Connection:
+    conn: pymysql.connections.Connection = connector.connect(
+        db_connection_name,
+        "pymysql",
+        user=db_user,
+        password=db_password,
+        db=db_name
+    )
+    print('conn successful')
 
-    return conn
-
+    return conn 
 
 def get_natural_disaster(d_name):
-    conn = open_connection()
-    with conn.cursor() as cursor:
-        query = f'SELECT * FROM NaturalDisaster WHERE name == {d_name} LIMIT 15;'
-        result = cursor.execute(query)
-        disasters = cursor.fetchall()
-        got_names = jsonify(disasters) if result > 0 else 'No matching events found.'
-    conn.close()
-    return got_names
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+    )
+    
+    with pool.connect() as db_conn:
+        query = f'SELECT * FROM NaturalDisaster WHERE name = "{d_name}" LIMIT 15;'
+        disasters = db_conn.execute(sqlalchemy.text(query)).fetchall()
+        for row in disasters:
+            print(row)
+    connector.close()
