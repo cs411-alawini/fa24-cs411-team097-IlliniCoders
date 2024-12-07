5a1f31fc31d53971cdf427343819155d89d26976
“madebayo1023” <Adebayo5@illinois.edu>
2024-12-07 14:56:42 -0600

    Added advanced queries and updated frontend.

--- currents/server/api/main.py
+++ currents/server/api/main.py
@@ -1,26 +1,37 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from db import get_natural_disaster 
-# import sys
-# sys.path.append('../')
-
-# from db import get_natural_disaster
+from db import get_advanced_query1 
+from db import get_advanced_query2
+from db import get_advanced_query3
+from db import get_advanced_query4
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 @app.route('/data', methods=['GET', 'POST'])
 def receive_data():
     if request.method == 'POST':
-        #data = request.data.decode("utf-8")
         data = request.get_json()
         print(data)
     
         if not data:
             return jsonify({"error": "No data found,"}), 404
     
-
-        return jsonify({"data": get_natural_disaster(data.get("query"))}), 200
+        elif data.get('advanced_query') == '1':
+            print("here")
+            return jsonify({"data": get_advanced_query1()}), 200
+        elif data.get('advanced_query') == '2':
+            print("here")
+            return jsonify({"data": get_advanced_query2()}), 200
+        elif data.get('advanced_query') == '3':
+            print("here")
+            return jsonify({"data": get_advanced_query3()}), 200
+        elif data.get('advanced_query') == '4':
+            print("here")
+            return jsonify({"data": get_advanced_query4()}), 200
+        # print("here outside")
+        return jsonify({"data": get_natural_disaster(data.get('min_latitude'), data.get('max_latitude'), data.get('min_longitude'), data.get('max_longitude'))}), 200
     
     if request.method == 'GET':
         return jsonify({"data": "hi"}), 200
